0eecafb7c23281cadf0c3c255453868a4878c18c
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-11-07 11:34:08 +0530

    Datatype

--- datatypesAndVariables.js
+++ datatypesAndVariables.js
@@ -19,3 +19,36 @@ let a=10
  //String
  let name = 'vidwath'
  let palce = "Hassan"
+
+ //BigInt
+ let num = 25651678435165468135178n
+
+ //null
+ let no = null
+
+ //undefined
+ let sum;
+
+ //Object
+ const employee = {
+    name : "Vidwath",
+    empId: 10154,
+    age: 22
+ }
+
+//Array
+const ar =[1,5,2,8]
+
+//Fuction
+function hello(){
+    console.log("Hello Everyone")
+}
+
+//Date
+const today = new Date(`07/11/2024`)
+
+//Map And Set
+const map = new Map();
+map.set("name", "Alice");
+
+const set = new Set([1, 2, 3, 3],[4,5,6,7]);
