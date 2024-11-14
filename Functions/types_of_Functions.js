a48361f753a2786304518d9d4825c539645069d1
Vidwath K T <vidwathveerashiva63@gmail.com>
2024-11-14 12:39:53 +0530

    [Vidwath]Added:Types of function

---
+++ Functions/types_of_Functions.js
@@ -0,0 +1,40 @@
+//1.Named Function
+function sayHello() {
+    console.log("Hello!");
+}
+sayHello(); // Calling the function
+
+//2.Anonymous Function
+const greet1 = function() {
+    console.log("Hello, anonymous!");
+};
+greet(); // Invoked via the variable holding the function
+
+//3.Arrow Fuction
+const add = (a, b) => a + b;
+console.log(add(5, 3)); // Output: 8
+//4.Function Expression
+const greet2 = function(name) {
+    console.log("Hello, " + name + "!");
+  };
+  
+  greet("Bob");
+
+//5.generator Function
+function* generatorFunc() {
+    yield 1;
+    yield 2;
+    yield 3;
+}
+
+const iterator = generatorFunc();
+console.log(iterator.next().value); // Output: 1
+
+//6.constructor function
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+}
+
+const person1 = new Person("Alice", 25);
+
